65f34d399acdabb9c5126577a5d51006ff28ae8c
Derek Chen <derekgc@icloud.com>
2018-05-22 10:53:53 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   resources :songs, only: [:index, :new, :show, :edit, :create, :destroy]
   patch 'songs/:id', to: 'songs#update'
+  get '/', to: 'songs#index'
 
 end
